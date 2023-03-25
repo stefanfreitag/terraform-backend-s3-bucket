9b23e01d1c5b2e3310551d38483861d8c4414f8f
Stefan Freitag <stefan.freitag@udo.edu>
2023-03-25 17:20:05 +0100

    feat:  setup lifecycle policy for s3 objects (#3)
    * test: added basic integration test code
    
    * feat: added lifecycle policy to S3 bucket
    
    * chore: upgrade cdk to v2.70.0
    
    ---------
    
    Co-authored-by: Stefan Freitag <stefan.freitag@rwe.com>

--- .projenrc.js
+++ .projenrc.js
@@ -5,15 +5,15 @@ const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Stefan Freitag',
   authorAddress: 'stefan.freitag@udo.edu',
-  cdkVersion: '2.69.0',
+  cdkVersion: '2.70.0',
   defaultReleaseBranch: 'main',
   name: 'terraform-backend-s3-bucket',
   repositoryUrl:
     'https://github.com/stefanfreitag/terraform-backend-s3-bucket.git',
   codeCov: true,
   devDeps: [
-    '@aws-cdk/integ-tests-alpha@2.69.0-alpha.0',
-    '@aws-cdk/integ-runner@2.69.0',
+    '@aws-cdk/integ-tests-alpha@2.70.0-alpha.0',
+    '@aws-cdk/integ-runner@2.70.0',
     'ts-node',
   ],
   depsUpgradeOptions: {
@@ -22,6 +22,15 @@ const project = new awscdk.AwsCdkConstructLibrary({
     },
   },
   stability: Stability.EXPERIMENTAL,
+  publishToMaven: {
+    javaPackage: 'io.github.stefanfreitag.cdk.terraformstatebackend',
+    mavenArtifactId: 'terraformStateBackend',
+    mavenGroupId: 'io.github.stefanfreitag',
+  },
+  publishToNuget: {
+    dotNetNamespace: 'Io.Github.StefanFreitag',
+    packageId: 'Io.Github.StefanFreitag.TerraformStateBackend',
+  },
   publishToPypi: {
     module: 'terraform_backend_s3_bucket',
     distName: 'terraform-backend-s3-bucket',
