49f7a9d61fdeef78deb2bba52cc2942be3a3b501
Stefan Freitag <stefan.freitag@udo.edu>
2023-07-20 11:01:24 +0200

    Upgrade dependencies and CDK version
    - Upgrade @aws-cdk/integ-runner to version 2.87.0-alpha.0
    - Upgrade @aws-cdk/integ-tests-alpha to version 2.87.0-alpha.0
    - Upgrade aws-cdk-lib to version 2.87.0
    - Upgrade cdk-nag to version 2.27.75
    - Update CDK version in .projenrc.js and package.json from 2.74.0 to 2.87.0

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,7 @@ const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Stefan Freitag',
   authorAddress: 'stefan.freitag@udo.edu',
-  cdkVersion: '2.74.0',
+  cdkVersion: '2.87.0',
   defaultReleaseBranch: 'main',
   name: 'terraform-backend-s3-bucket',
   description: 'Creates an S3 bucket and a DynamoDB table for Terraform state and lock management.',
@@ -13,14 +13,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'https://github.com/stefanfreitag/terraform-backend-s3-bucket.git',
   codeCov: true,
   devDeps: [
-    '@aws-cdk/integ-tests-alpha@2.74.0-alpha.0',
-    '@aws-cdk/integ-runner@2.74.0-alpha.0',
-    'cdk-nag@2.25.12',
+    '@aws-cdk/integ-tests-alpha@2.87.0-alpha.0',
+    '@aws-cdk/integ-runner@2.87.0-alpha.0',
+    'cdk-nag@2.27.75',
     'ts-node',
   ],
   depsUpgradeOptions: {
     workflowOptions: {
-      schedule: UpgradeDependenciesSchedule.WEEKLY,
+      schedule: UpgradeDependenciesSchedule.MONTHLY,
     },
   },
   stability: Stability.EXPERIMENTAL,
