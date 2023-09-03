571e0e6dcb107f71d709a86f8175f991669a4c6f
Stefan Freitag <stefan.freitag@rwe.com>
2023-09-03 14:07:00 +0200

    chore: upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,7 @@ const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Stefan Freitag',
   authorAddress: 'stefan.freitag@udo.edu',
-  cdkVersion: '2.88.0',
+  cdkVersion: '2.94.0',
   defaultReleaseBranch: 'main',
   name: 'terraform-backend-s3-bucket',
   description: 'Creates an S3 bucket and a DynamoDB table for Terraform state and lock management.',
@@ -13,9 +13,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'https://github.com/stefanfreitag/terraform-backend-s3-bucket.git',
   codeCov: true,
   devDeps: [
-    '@aws-cdk/integ-tests-alpha@2.88.0-alpha.0',
-    '@aws-cdk/integ-runner@2.88.0-alpha.0',
-    'cdk-nag@2.27.75',
+    '@aws-cdk/integ-tests-alpha@2.94.0-alpha.0',
+    '@aws-cdk/integ-runner@2.94.0-alpha.0',
+    'cdk-nag@2.27.120',
     'ts-node',
   ],
   depsUpgradeOptions: {
